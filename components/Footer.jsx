66daadae5e5d11d12f849832e5cf95fb0ab1281e
SHIVANGI KOTNALA <91424058+sphoenix-sk@users.noreply.github.com>
2023-02-20 13:35:44 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Box, Link } from '@chakra-ui/react';
+
+
+const Footer = () => (
+    <Box>
+    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
+        2023 GharDekho™, Inc. 
+    </Box>
+    <Box textAlign='center' p='5' color='gray.600'>
+</Box>
+</Box>
+)
+export default Footer;
